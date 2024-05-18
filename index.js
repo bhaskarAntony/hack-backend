5f8101943b96cd1426b86e91c3374dc01b181103
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-05-18 23:21:58 +0530

    updated files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path'); // Import the path module
 const cors = require('cors')
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.delete('/deleteFile', (req, res) => {
     const { filePath } = req.body;
